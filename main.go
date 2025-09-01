2089ebe943591e6db86ebc4799938157eec183cb
Doru Budai <doru.budai@gmail.com>
2025-09-01 12:45:37 +0300

    Create user

--- main.go
+++ main.go
@@ -11,14 +11,17 @@ import (
 	"os"
 	"strings"
 	"sync/atomic"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
-	db             *database.Queries
+	sql            *database.Queries
+	platform       string
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -55,7 +58,49 @@ func validateChirp(w http.ResponseWriter, r *http.Request) {
 
 	cleanedBody := replaceProfane(params.Body)
 
-	writeSuccessJson(w, cleanedBody)
+	type response struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+	resp := response{CleanedBody: cleanedBody}
+
+	writeSuccessJson(w, resp)
+}
+
+func (cfg *apiConfig) createUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	type parameters struct {
+		Email string `json:"email"`
+	}
+
+	var params parameters
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		writeErrorJson(w, err, "Something went wrong")
+		return
+	}
+
+	email := strings.TrimSpace(params.Email)
+	if len(email) < 5 {
+		writeErrorJson(w, errors.New("email is too short"), "Email is too short")
+		return
+	}
+
+	user, err := cfg.sql.CreateUser(r.Context(), email)
+	if err != nil {
+		writeErrorJson(w, err, "Couldn't create user")
+		return
+	}
+
+	type response struct {
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Email     string    `json:"email"`
+	}
+	resp := response{user.ID, user.CreatedAt, user.UpdatedAt, user.Email}
+
+	writeSuccessJson(w, resp, http.StatusCreated)
 }
 
 func writeErrorJson(w http.ResponseWriter, err error, message ...string) {
@@ -70,13 +115,12 @@ func writeErrorJson(w http.ResponseWriter, err error, message ...string) {
 	_ = json.NewEncoder(w).Encode(resp)
 }
 
-func writeSuccessJson(w http.ResponseWriter, message ...string) {
-	type response struct {
-		CleanedBody string `json:"cleaned_body"`
+func writeSuccessJson(w http.ResponseWriter, resp any, statusCode ...int) {
+	if statusCode != nil {
+		w.WriteHeader(statusCode[0])
+	} else {
+		w.WriteHeader(http.StatusOK)
 	}
-	resp := response{CleanedBody: strings.Join(message, " ")}
-
-	w.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(w).Encode(resp)
 }
 
@@ -104,7 +148,18 @@ func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
 	cfg.fileserverHits.Store(0)
+
+	err := cfg.sql.DeleteUsers(r.Context())
+	if err != nil {
+		writeErrorJson(w, err, "Couldn't truncate users")
+		return
+	}
 }
 
 func main() {
@@ -112,6 +167,8 @@ func main() {
 
 	const filepathRoot = "."
 	const port = "8080"
+
+	platform := os.Getenv("PLATFORM")
 
 	dbURL := os.Getenv("DB_URL")
 	db, err := sql.Open("postgres", dbURL)
@@ -121,21 +178,23 @@ func main() {
 
 	dbQueries := database.New(db)
 
-	apiCfg := &apiConfig{
+	cfg := &apiConfig{
 		fileserverHits: atomic.Int32{},
-		db:             dbQueries,
+		sql:            dbQueries,
+		platform:       platform,
 	}
 
 	mux := http.NewServeMux()
 
 	handle := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handle))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(handle))
 
 	mux.HandleFunc("GET /api/healthz", healthz)
 	mux.HandleFunc("POST /api/validate_chirp", validateChirp)
+	mux.HandleFunc("POST /api/users", cfg.createUser)
 
-	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
-	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	mux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", cfg.resetHandler)
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 
